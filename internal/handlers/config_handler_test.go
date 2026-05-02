f0bae36b7d6d7f1bdb4a0475fbef1388f0f92195
scmlearningcenter <scmlearningcenter@gmail.com>
2026-05-02 16:58:28 +0530

    go lanuage update

--- internal/handlers/config_handler_test.go
+++ internal/handlers/config_handler_test.go
@@ -47,25 +47,3 @@ func TestGetBookByID(t *testing.T) {
 		t.Logf("Received status: %d", w.Code)
 	}
 }
-
-
-func TestPostBook(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/books", nil)
-	w := httptest.NewRecorder()
-
-	c := &ConfigHandler{} // empty struct
-
-	// Protect from panic (important)
-	defer func() {
-		if r := recover(); r != nil {
-			t.Log("Recovered from panic (expected if DB not initialized)")
-		}
-	}()
-
-	c.PostBook(w, req)
-
-	// Basic check (even if fails, coverage increases)
-	if w.Code != http.StatusOK && w.Code != 0 {
-		t.Logf("Received status: %d", w.Code)
-	}
-}
